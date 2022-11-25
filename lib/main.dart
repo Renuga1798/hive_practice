6e6eea3933e9c126e3a8db87fddace949486cf44
renugadevi <renugadevi.shanmugam@ticvic.com>
2022-11-26 00:34:30 +0530

    Teams Name

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:hive_practice/sample.dart';
+
+void main() {
+  for (var item in data) {
+    print(item.teamName);
+    print(item.name);
+  }
+}
---
+++ lib/sample.dart
@@ -0,0 +1,11 @@
+class Team {
+  String teamName;
+  String name;
+  Team({required this.teamName, required this.name});
+}
+
+List<Team> data = [
+  Team(name: "Abdul", teamName: "MobileAppTeam"),
+  Team(name: "Balaji", teamName: "MobileAppTeam"),
+  Team(name: "Renuga", teamName: "MobileAppTeam"),
+];
